ab469cb8a6882ab58a9221a92b10bd57c1f1adec
Matheus Mendes <matheus@zeroflows.com>
2020-12-22 14:32:26 +0000

    reject status sending

--- src/main/java/com/hes/zf/fix/server/message/handlers/ReportEventHandler.java
+++ src/main/java/com/hes/zf/fix/server/message/handlers/ReportEventHandler.java
@@ -1,15 +1,20 @@
 package com.hes.zf.fix.server.message.handlers;
 
 import com.hes.zf.fix.server.message.events.NewOrderCreatedEvent;
+import com.hes.zf.fix.server.service.ExecutionReportService;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import quickfix.field.OrdStatus;
 
-//@Component
-public class ReportEventHandler {
-//        implements ApplicationListener<NewOrderCreatedEvent> {
+@Component
+public class ReportEventHandler implements ApplicationListener<NewOrderCreatedEvent> {
 
-//    @Async
-//    @EventListener
+    @Async
+    @EventListener
     public void onApplicationEvent(NewOrderCreatedEvent newOrderCreatedEvent) {
 
-//        ExecutionReportService.send(newOrderCreatedEvent.getNewOrderSingle());
+        ExecutionReportService.send(newOrderCreatedEvent.getNewOrderSingle(), new OrdStatus(OrdStatus.REJECTED));
     }
 }
