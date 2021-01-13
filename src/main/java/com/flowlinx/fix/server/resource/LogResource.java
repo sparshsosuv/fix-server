18bf6243470159a7d3cf0da2c39ddcdf408a3726
Maicon Borges <maicon.borges@genesis.global>
2021-01-13 16:24:41 -0300

    FIX-20

--- src/main/java/com/flowlinx/fix/server/resource/LogResource.java
+++ src/main/java/com/flowlinx/fix/server/resource/LogResource.java
@@ -3,13 +3,14 @@ package com.flowlinx.fix.server.resource;
 import com.flowlinx.fix.server.persistence.FixMessage;
 import com.flowlinx.fix.server.persistence.FixMessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @CrossOrigin(maxAge = 3600)
 @Controller
@@ -19,9 +20,14 @@ public class LogResource {
     @Autowired
     private FixMessageRepo fixMessageRepo;
 
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE )
+    @GetMapping( produces = MediaType.APPLICATION_JSON_VALUE )
     public ResponseEntity<Iterable<FixMessage>> logs() {
-        return ResponseEntity.ok().body( fixMessageRepo.findAll( Sort.by("id").descending() ) );
+
+        final PageRequest pageable = PageRequest.of( 0, 100, Sort.by("id").descending() );
+
+        final Iterable<FixMessage> messages = fixMessageRepo.findAll( pageable );
+
+        return ResponseEntity.ok().body( messages );
     }
 
 }
