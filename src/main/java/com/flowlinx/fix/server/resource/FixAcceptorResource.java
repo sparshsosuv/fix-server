ac1db444b712bfd4507145b4e4a1f2f5dd461c01
Maicon Borges <maicon@softcontext.com>
2021-08-17 18:51:02 -0300

    dynamic sessions

--- src/main/java/com/flowlinx/fix/server/resource/FixAcceptorResource.java
+++ src/main/java/com/flowlinx/fix/server/resource/FixAcceptorResource.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import quickfix.Acceptor;
 import quickfix.Session;
 import quickfix.SessionID;
 import quickfix.ThreadedSocketAcceptor;
