b3cf26713fbe28ad7acd43e7179cf499b67afebd
Maicon Borges <maicon@softcontext.com>
2021-12-01 20:11:42 -0300

    adding logs

--- src/main/java/com/flowlinx/fix/server/message/event/handler/RouteEventHandler.java
+++ src/main/java/com/flowlinx/fix/server/message/event/handler/RouteEventHandler.java
@@ -6,8 +6,6 @@ import com.flowlinx.fix.server.type.FixTargetSession;
 import com.flowlinx.fix.server.utils.AppUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.dozer.Mapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import quickfix.Message;
@@ -21,36 +19,31 @@ import java.util.Optional;
 @Component
 public class RouteEventHandler implements ApplicationListener<RouteEvent> {
 
-    @Autowired
-    private Mapper mapper;
-
     @Override
     public void onApplicationEvent(RouteEvent event) {
 
         try{
-            final Message message = mapper.map( event.getMessage(), Message.class ) ;
+            final Message message = event.getMessage();
             final RoutingTable route = event.getRoutingTable();
 
-
-            final String targetCompID =  AppUtils.getString( message.getHeader(), TargetCompID.FIELD );
             final String senderCompID =  AppUtils.getString( message.getHeader(), SenderCompID.FIELD );
             message.getHeader().setField( new OnBehalfOfCompID( senderCompID ) );
 
             log.info("Routing message FROM: {}, TO: {}", route.getSenderCompID(), route.getTargetCompID()  );
-            log.info("ROUTE: ", route.toString()  );
             log.info("MESSAGE: {}",  message.toString() );
 
             final boolean result = Session.sendToTarget( message, route.getSenderCompID(), route.getTargetCompID() );
 
             if(!result){
 
-                final Message reject = mapper.map( event.getMessage(), Message.class ) ;
-                reject.setField( new ExecType( ExecType.REJECTED ) );
-                reject.setField( new OrdStatus( OrdStatus.REJECTED ));
-                reject.setField( new TransactTime( LocalDateTime.now() ) );
-                reject.setField( new Text( StringUtils.replace( route.getTargetCompID() + " is currently offline", " ", "_" )  ) );
+                message.getHeader().removeField( OnBehalfOfCompID.FIELD );
+                message.setField( new ExecType( ExecType.REJECTED ) );
+                message.setField( new OrdStatus( OrdStatus.REJECTED ));
+                message.setField( new TransactTime( LocalDateTime.now() ) );
+                message.setField( new Text( StringUtils.replace( route.getTargetCompID() + " is offline", " ", "_" )  ) );
 
-                Session.sendToTarget( reject, targetCompID, senderCompID );
+                final String targetCompID =  AppUtils.getString( message.getHeader(), TargetCompID.FIELD );
+                Session.sendToTarget( message, targetCompID, senderCompID );
             }
 
         } catch (Exception ex) {
