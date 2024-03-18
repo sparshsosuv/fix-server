2c376559da492161bd4bc2d74c3fbdc5ce7e7ac4
Maicon Borges <maicon@softcontext.com>
2024-03-18 12:39:11 -0300

    adding installation steps

---
+++ src/main/resources/Readme.md
@@ -0,0 +1,30 @@
+
+# Fix-server
+
+It handles fix connectivity.
+
+## SETTING UP DATABASE
+1. INSTALL POSTGRES 9+;
+   1.1 - Enable MD2
+      /etc/postgresql/14/main/pg_hba.conf
+2. RUN SCRIPT ON PSQL FOR CREATING THE APPS DATABASE;
+   CREATE DATABASE quickfix;
+2. UPDATE USER AND PASSWORD.
+   2.1 FILE: resources/applications.properties
+      datasource.url=jdbc:postgresql://localhost:5432/quickfix?socketTimeout=30
+      datasource.username=postgres
+      datasource.password=postgres
+3. FIX DATABASE INTEGRATION
+   3.1. FILE: resources/fix/quickfixj-server-uat.cfg
+   JdbcDriver=org.postgresql.Driver
+   JdbcURL=jdbc:postgresql://localhost:5432/quickfix?socketTimeout=30
+   JdbcUser=postgres
+   JdbcPassword=postgres
+
+
+#### Main configurations
+
+
+quickfixj-server-uat.cfg
+
+
---
+++ src/main/resources/flowlinx-fix-server.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+JARFile="/opt/flowlinx/apps/flowlinx-fix-server/target/flowlinx-fix-server.jar"
+PIDFile="/opt/flowlinx/pid/flowlinx-fix-server.pid"
+JVM_OPTS="-Xdebug -Xmx2g -Dspring.profiles.active=uat"
+SPRING_OPTS=""
+
+function check_if_pid_file_exists {
+    if [ ! -f $PIDFile ]
+    then
+ echo "PID file not found: $PIDFile"
+      # exit 1
+    fi
+}
+
+function check_if_process_is_running {
+ if ps -p $(print_process) > /dev/null
+ then
+     return 0
+ else
+     return 1
+ fi
+}
+
+function print_process {
+    echo $(<"$PIDFile")
+}
+
+case "$1" in
+  status)
+    check_if_pid_file_exists
+    if check_if_process_is_running
+    then
+      echo $(print_process)" is running"
+    else
+      echo "Process not running: $(print_process)"
+    fi
+    ;;
+  stop)
+    check_if_pid_file_exists
+    if ! check_if_process_is_running
+    then
+      echo "Process $(print_process) already stopped"
+      exit 0
+    fi
+    kill -TERM $(print_process)
+    echo -ne "Waiting for process to stop"
+    NOT_KILLED=1
+    for i in {1..20}; do
+      if check_if_process_is_running
+      then
+        echo -ne "."
+        sleep 1
+      else
+        NOT_KILLED=0
+      fi
+    done
+    echo
+    if [ $NOT_KILLED = 1 ]
+    then
+      echo "Cannot kill process $(print_process)"
+      exit 1
+    fi
+    echo "Process stopped"
+    ;;
+  start)
+    if [ -f $PIDFile ] && check_if_process_is_running
+    then
+      echo "Process $(print_process) already running"
+      exit 1
+    fi
+    nohup java $JVM_OPTS -jar $JARFile </dev/null>/opt/flowlinx/logs/flowlinx-fix-server-run.log 2>&1 & $SPRING_OPTS &
+    echo "Process started"
+    ;;
+  restart)
+    $0 stop
+    if [ $? = 1 ]
+    then
+      exit 1
+    fi
+    $0 start
+    ;;
+  *)
+    echo "Usage: $0 {start|stop|restart|status}"
+    exit 1
+esac
+
+exit 0
